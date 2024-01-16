05a4dbe142336018f230aab4f55b27a28e6aeac2
EdithRW <edith.saldana@rulesware.com>
2024-01-16 14:11:25 -0600

    initial code for swagger documentation

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const express = require("express");
+const express = require("express"),
+bodyParser = require("body-parser"),
+swaggerJsdoc = require("swagger-jsdoc"),
+swaggerUi = require("swagger-ui-express");
+
+
 require('dotenv').config();
 const cors = require('cors');
 
@@ -24,6 +29,45 @@ dbConnection();
 app.use('/api/users', require('./routes/users'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/upload', require('./routes/uploads'));
+
+
+//SWAGGER CONFIG
+const options = {
+    definition: {
+      openapi: "3.1.0",
+      info: {
+        title: "RW Studio Express API with Swagger",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "LogRocket",
+          url: "https://logrocket.com",
+          email: "info@email.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:8000",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+  };
+  
+  const specs = swaggerJsdoc(options);
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
+
+//END OF SWAGGER CONFIG
+  
 
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,30 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication managing API
+ * /:
+ *   post:
+ *     summary: Log In
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The created book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Some server error
+ *
+ */
+
 const { Router } = require('express');
 const {login, loginGoogle, refreshToken} = require('../controllers/auth');
 const { validateJWT } = require('../middlewares/token-validation');
